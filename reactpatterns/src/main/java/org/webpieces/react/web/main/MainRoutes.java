82f595a8b4f42a59c2580ff8f356e1f12c692350
Dean <dean@Deans-MacBook-Pro.local>
2022-02-23 07:49:27 -0700

    modifications for example react app

--- reactpatterns/src/main/java/org/webpieces/react/web/main/MainRoutes.java
+++ reactpatterns/src/main/java/org/webpieces/react/web/main/MainRoutes.java
@@ -6,6 +6,7 @@ import static org.webpieces.react.web.main.MainRouteId.MAIN_ROUTE;
 
 import org.webpieces.router.api.routebldr.DomainRouteBuilder;
 import org.webpieces.router.api.routebldr.RouteBuilder;
+import org.webpieces.router.api.routebldr.ScopedRouteBuilder;
 import org.webpieces.router.api.routes.Routes;
 
 public class MainRoutes implements Routes {
@@ -17,16 +18,14 @@ public class MainRoutes implements Routes {
 		//   regex a capture group for you BUT leave the rest untouched so you can do whatever regex you like
 		//   ORDER matters so the order of modules is important and the order of routes is important
 
-		//The Controller.method is a relative or absolute path with ClassName.method at the end
-		//RouteIds are used to redirect in the webapp itself and must be unique
-		bldr.addRoute(BOTH, GET, "/",              "MainController.index", MAIN_ROUTE);
+		bldr.addStaticFile(BOTH, "/", "react/index.html", false);
+
+		bldr.addStaticDir(BOTH, "/", "react/", false);
 
 		bldr.addStaticDir(BOTH, "/assets/", "public/", false);
 		//Add a single file by itself(not really needed)
 		bldr.addStaticFile(BOTH, "/favicon.ico", "public/favicon.ico", false);
 		bldr.addStaticFile(BOTH, "/test.css", "public/crud/fonts.css", false);
-
-		bldr.addStaticDir(BOTH, "/", "react/", false);
 
 		bldr.setPageNotFoundRoute("MainController.notFound");
 		bldr.setInternalErrorRoute("MainController.internalError");
