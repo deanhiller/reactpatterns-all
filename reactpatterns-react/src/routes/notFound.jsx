2e4dc7c1fda6820fe79b5f2fe2fad3b5be0b145d
Dean <dean@Deans-MacBook-Pro.local>
2022-02-25 12:51:50 -0700

    fix up react routes as well as page not found stuff

---
+++ reactpatterns-react/src/routes/notFound.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import { getInvoices } from "../data";
+
+export default function NotFound() {
+    return (
+        <div style={{ display: "flex" }}>
+            Page Not Found
+        </div>
+    );
+}
--- reactpatterns/src/main/java/org/webpieces/react/web/main/MainRoutes.java
+++ reactpatterns/src/main/java/org/webpieces/react/web/main/MainRoutes.java
@@ -18,14 +18,11 @@ public class MainRoutes implements Routes {
 		//   regex a capture group for you BUT leave the rest untouched so you can do whatever regex you like
 		//   ORDER matters so the order of modules is important and the order of routes is important
 
-		bldr.addStaticFile(BOTH, "/", "react/index.html", false);
-
-		bldr.addStaticDir(BOTH, "/", "react/", false);
-
 		bldr.addStaticDir(BOTH, "/assets/", "public/", false);
-		//Add a single file by itself(not really needed)
-		bldr.addStaticFile(BOTH, "/favicon.ico", "public/favicon.ico", false);
-		bldr.addStaticFile(BOTH, "/test.css", "public/crud/fonts.css", false);
+
+		bldr.addStaticFile(BOTH, "/", "react/index.html", false);
+		bldr.addStaticDir(BOTH, "(?<resource>.*\\..*)", "react/", false);
+		bldr.addStaticFile(BOTH, "/.*", "react/index.html", false);
 
 		bldr.setPageNotFoundRoute("MainController.notFound");
 		bldr.setInternalErrorRoute("MainController.internalError");
