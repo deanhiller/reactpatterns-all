de6e5ea99e770a9e566c5c9fa2f9242fa722d6e6
patrick <patrick@orderlyhealth.com>
2022-02-26 14:44:08 -0700

    userSvc Implenetations

---
+++ reactpatterns-react/src/api/usersvc/User.ts
@@ -0,0 +1,10 @@
+export default interface User {
+
+    getId(): number
+    setId(): void
+    getFirstName(): string
+    setFirstName(): void
+    getLastName(): string
+    setLastName(): void
+
+}
