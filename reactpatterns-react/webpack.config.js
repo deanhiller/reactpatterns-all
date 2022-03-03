c281d6255109b16355a2f8d105f0ae2a965cee75
Dean <dean@Deans-MacBook-Pro.local>
2022-03-02 18:01:47 -0700

    make the delete button also work in webpack.  did not know webpack generated a reference to main.js as well

--- reactpatterns-react/webpack.config.js
+++ reactpatterns-react/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "none",
+  mode: "development",
   entry: "./src/index.tsx",
   devtool: "inline-source-map",
   devServer: {
