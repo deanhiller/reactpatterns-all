c386ef7ee1693e7b5013eef48e78890610d0b108
Dean <dean@Deans-MacBook-Pro.local>
2022-03-06 10:24:29 -0700

    fix formatting real quick

--- reactpatterns-react/src/routes/private/user/dashboard.tsx
+++ reactpatterns-react/src/routes/private/user/dashboard.tsx
@@ -3,40 +3,40 @@ import {Outlet, Link} from "react-router-dom";
 import {Auth0ContextInterface, useAuth0, User} from "@auth0/auth0-react";
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    export default function Dashboard() {
-        const { logout } = useAuth0();
+export default function Dashboard() {
+    const { logout } = useAuth0();
 
-        const { user, isAuthenticated, isLoading }: Auth0ContextInterface<User> = useAuth0();
+    const { user, isAuthenticated, isLoading }: Auth0ContextInterface<User> = useAuth0();
 
-        if (isLoading) {
-            return <div>Loading ...</div>;
-        }
-
-        return (
-        <div>
-            <h1>Bookkeeper</h1>
-            <nav
-                style={{
-                    borderBottom: "solid 1px",
-                    paddingBottom: "1rem"
-                }}
-            >
-                <Link to="/invoices">Invoices</Link> |{" "}
-                <Link to="/expenses">Expenses</Link>
-                <button onClick={() => logout({ returnTo: window.location.origin })}>
-                    Log Out
-                </button>
-                {
-                    user !== undefined && isAuthenticated && (
-                        <div>
-                            <img src={user.picture} alt={user.name} />
-                            <h2>{user.name}</h2>
-                            <p>{user.email}</p>
-                        </div>
-                    )
-                }
-            </nav>
-            <Outlet />
-        </div>
-    );
+    if (isLoading) {
+        return <div>Loading ...</div>;
     }
+
+    return (
+    <div>
+        <h1>Bookkeeper</h1>
+        <nav
+            style={{
+                borderBottom: "solid 1px",
+                paddingBottom: "1rem"
+            }}
+        >
+            <Link to="/invoices">Invoices</Link> |{" "}
+            <Link to="/expenses">Expenses</Link>
+            <button onClick={() => logout({ returnTo: window.location.origin })}>
+                Log Out
+            </button>
+            {
+                user !== undefined && isAuthenticated && (
+                    <div>
+                        <img src={user.picture} alt={user.name} />
+                        <h2>{user.name}</h2>
+                        <p>{user.email}</p>
+                    </div>
+                )
+            }
+        </nav>
+        <Outlet />
+    </div>
+);
+}
